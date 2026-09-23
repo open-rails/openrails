f710b2e25efb82ffaeba0997fb62f7a56c31e948
Paul Fidika <paul@fidika.com>
2026-09-23 11:45:08 -0600

    test: mark store NMI fixtures as explicit loopback sandboxes

--- internal/modules/money/merchant_store_collection_integration_test.go
+++ internal/modules/money/merchant_store_collection_integration_test.go
@@ -33,7 +33,7 @@ import (
 func storeCollectionTestConfig() *config.Config {
 	// TestMode=sandbox → provider environment "test", matching
 	// merchantsServiceForTest / seedPSPSecrets.
-	return &config.Config{TestMode: config.CredentialPostureSandbox, ProviderWriteMode: config.ProviderWriteModeFull}
+	return &config.Config{TestMode: config.CredentialPostureSandbox, ProviderWriteMode: config.ProviderWriteModeFull, ProviderSandbox: &config.ProviderSandboxConfig{NMIGatewayURL: "http://127.0.0.1:1"}}
 }
 
 func storeArmedCharger(dbi *db.DB, msvc *merchants.Service, boot map[string]money.CollectionAdapter, endpoints money.CollectionEndpoints) *money.ScopedCharger {
